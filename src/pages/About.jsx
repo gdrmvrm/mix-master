e8e1a2e1dd4979736c6758757ced6f3fa84afd3b
gdrmvrm <gdrmvrm@gmail.com>
2025-11-20 16:43:14 +0000

    Setting up links between home and about pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const About = () => {
-  return <h2>About</h2>;
+  return (
+    <div>
+      <h2>About</h2>
+      <Link to="/">Home</Link>
+    </div>
+  );
 };
 
 export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return <h2>Home</h2>;
+  return (
+    <div>
+      <h2>Home</h2>
+      <Link to="/about">About</Link>
+    </div>
+  );
 };
 
 export default Home;
